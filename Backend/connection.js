5150ac644508defe50e8aa98beabb1dd9c00fc5d
Jarrod McAtee <40642746+JMcatee25@users.noreply.github.com>
2018-11-02 15:51:11 -0400

    Delete connection.js

--- Backend/connection.js
+++
@@ -1,6 +0,0 @@
-var connection = mysql.createConnection({
-  host: "road2hire.ninja",
-  user: "r2hstudent", //your username
-  password: "SbFaGzNgGIE8kfP",
-  database: "jmcatee" //the name of your db
-});
