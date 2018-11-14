69c827b6da42df8691b971f384e8edabd53a8733
Jarrod Mcatee <jarrodmcatee25@gmail.com>
2018-11-14 12:26:54 -0500

    Fixes security risk

--- Backend/MySQLsever.js
+++ Backend/MySQLsever.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var cors = require("cors");
 const app = express();
-require("./db/connection");
+const connection = require("./db/connection");
 
 app.use(cors());
 
